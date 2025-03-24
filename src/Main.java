846d3c593fcc4ca99f2e3c81a6c0b297922db0b6
JVRKEA <juri0002@stud.kea.dk>
2025-03-24 10:19:16 +0100

    first

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,8 @@ public class Main{
 
 
         Pizza pizza = new Pizza("Vesuvio", "tomatsauce, ost, skinke og oregano", 57);
-                Pizza pizza = new Pizza("Amerikaner", "tomatsauce, ost, oksefars og oregano", 53);
-                Pizza pizza = new Pizza("Cacciatore", "tomatsauce, ost, pepperoni og oregano", 57);
+                new Pizza("Amerikaner", "tomatsauce, ost, oksefars og oregano", 53);
+                new Pizza("Cacciatore", "tomatsauce, ost, pepperoni og oregano", 57);
                 new Pizza("Carbona", "tomatsauce, ost, kødsauce, spaghetti, cocktailpølser og oregano", 63);
                 new Pizza("Dennis", "tomatsauce, ost, skinke, pepperoni, cocktailpølser og oregano", 65);
                 new Pizza("Bertil", "tomatsauce, ost, bacon og oregano", 57);
